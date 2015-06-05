3cd67ad27880109805a37fd9d53730c38cd0f589
Jan Tatje <jan@jnt.io>
2015-06-06 01:42:45 +0200

    Cleaned up json2.php further and fixed the date.

--- json2.php
+++ json2.php
@@ -1,18 +1,10 @@
 <?php
-// Vetretungsplandaten in JSON String umwandeln
-//Jochen Hillenstedt
-//10.12.2014
-//JSON:
-//{"vertretungen":[{"datum":<Datum>,
-//                 "daten":[<eintrag1>,
-//                          <eintrag2> ...]}
-//                ]}
-
+//Original von Jochen Hillenstedt 10.12.2014
 //Aenderungen von Jan Tatje
 
 // Hilfsmodul "simple html dom" einbinden (Auswerten des DOM)
 include('./inc/simple_html_dom.php');
-//Spalten der html Datei einzeln speichern
+
 $VTP = Array( 
 		"vertretungen" => Array(
 			"tag1" => Array(
@@ -31,90 +23,71 @@ $VTP = Array(
 		"info" => ""
 );
 
-$URL = 'http://willms-gymnasium.selfhost.bz:88/svplan.html'; //Adresse des Vertretungsplans
-$zaehler = 0; //Ab welcher Zeile soll der Quelltext verarbeitet werden
-
 //encoding
 $src_charset = "Windows-1252";
+//other
+$days = 3;
 
 //Username und Password 
 $username = 'schueler@willms';
 $password = 'SpiegleinGmbH';
-$userpw = $username.':'.$password;
-
-$i = 0;
 
 // Daten einlesen mit cURL
 // direktes Einlesen ist auf diesem Server nicht möglich
 $curl = curl_init();
-curl_setopt($curl, CURLOPT_URL, $URL);
-curl_setopt($curl, CURLOPT_USERPWD, $userpw); //Daten aus .htaccess
+//general options
+curl_setopt($curl, CURLOPT_USERPWD, $username.':'.$password); //Daten aus .htaccess
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 10);
-
-$str = curl_exec($curl);
+// get vtp, convert to utf-8 immediately
+curl_setopt($curl, CURLOPT_URL, "http://willms-gymnasium.selfhost.bz:88/svplan.html");
+$vtp_str = iconv($src_charset, "UTF-8", curl_exec($curl));
+// get info
+curl_setopt($curl, CURLOPT_URL, 'http://willms-gymnasium.selfhost.bz:88/info.html');  
+$info_str = iconv($src_charset, "UTF-8", curl_exec($curl));
 curl_close($curl);
 
-//Daten auch curl übernehmen
-$html= str_get_html(iconv($src_charset, "UTF-8", $str));
-
-
+$html = str_get_html($vtp_str);
 //Tabelle durcharbeiten mit simple html dom und in Listen umwandeln
+$i = 0;
 foreach($html->find('tr') as $zeile) {
-	$temp = $zeile->find('td',0);
 	if ($i == 2) {
-		$VTP["vertretungen"]["tag1"]["datum"] = str_replace('&nbsp','',$temp->plaintext);
-		$VTP["vertretungen"]["tag2"]["datum"] = str_replace('&nbsp','',$temp->plaintext);
-		$VTP["vertretungen"]["tag3"]["datum"] = str_replace('&nbsp','',$temp->plaintext);
+		$VTP["vertretungen"]["tag1"]["datum"] =
+		str_replace('&nbsp','',$zeile->find('td',0)->plaintext);
+		$VTP["vertretungen"]["tag2"]["datum"] =
+		str_replace('&nbsp','',$zeile->find('td',1)->plaintext);
+		$VTP["vertretungen"]["tag3"]["datum"] =
+		str_replace('&nbsp','',$zeile->find('td',2)->plaintext);
 	}
 	if ($i++ <= 2) continue;
-
-	if (trim($temp->plaintext) <> '.') 
-		array_push($VTP["vertretungen"]["tag1"]["daten"], str_replace('&nbsp','',$temp->plaintext));
-	$temp = $zeile->find('td',1);
-	if (trim($temp->plaintext) <> '.')
-		array_push($VTP["vertretungen"]["tag2"]["daten"], str_replace('&nbsp','',$temp->plaintext));
-	$temp = $zeile->find('td',2);
-	if (trim($temp->plaintext) <> '.') 
-		array_push($VTP["vertretungen"]["tag3"]["daten"], str_replace('&nbsp','',$temp->plaintext));
+	for ($j = 0; $j < $days; $j++) {
+		$temp = $zeile->find('td',$j);
+		if (trim($temp->plaintext) <> '.') 
+			array_push($VTP["vertretungen"]["tag".($j+1)]["daten"], str_replace('&nbsp','',$temp->plaintext));
+	}
 }
 
-
-//Liste der Spalten abschließen
-//Komma nach dem letzten Eintrag entfernen
-/*$spalte1 = substr($spalte1,0,-1).']},';
-$spalte2 = substr($spalte2,0,-1).']},';
-$spalte3 = substr($spalte3,0,-1).']},';
-*/
-//Info-Fenster
-// Daten einlesen mit cURL
-$curl = curl_init(); 
-curl_setopt($curl, CURLOPT_URL, 'http://willms-gymnasium.selfhost.bz:88/info.html');  
-curl_setopt($curl, CURLOPT_USERPWD, $userpw);
-curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);  
-curl_setopt($curl, CURLOPT_CONNECTTIMEOUT, 10);  
-
-$str = curl_exec($curl);  
-curl_close($curl);  
-
 //Inhalt des Infofensters auslesen
-$html = str_get_html($str); 
-$temp = $html->find('html',0);
-$memotext = $temp->innertext;
+$html = str_get_html($info_str);
+$memotext = $html->find('html',0)->innertext;
 $memotext = str_replace("\r\n", "", $memotext);
-$memotext = str_replace(chr(34), chr(39), $memotext);
+$memotext = str_replace(chr(34), chr(39), $memotext); // what does this do???
 $memotext = trim($memotext);
-$VTP["info"] = iconv($src_charset, "UTF-8", strip_tags($memotext));
+
+$VTP["info"] = strip_tags($memotext);
 
 //CORS header setzen
 header("Access-Control-Allow-Origin: *");
 
 //Daten-Ausgabe
-
 $json = json_encode($VTP, JSON_PRETTY_PRINT|JSON_UNESCAPED_UNICODE);
 if (!$json)
-	die("fatal error was fatal.");
-
+	$json = json_encode(
+		Array(
+			"error" => "encode_json() failed"
+		),
+		JSON_PRETTY_PRINT|JSON_UNESCAPED_UNICODE
+	);
 echo $json;
-//ENDE
 ?>
+
